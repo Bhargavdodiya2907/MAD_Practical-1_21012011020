3dbcec39307cafc810421fa67b97cec179ce53e0
student <student@gnu.ac.in>
2023-07-20 07:53:34 +0530

    Initial commit

---
+++ src/main/kotlin/practical1.3.kt
@@ -0,0 +1,27 @@
+fun main()
+{
+    var a="Student Name"
+    println("$a")
+    var b=readLine()
+    println("$b")
+
+    var a1="Student Enrollment Number"
+    println("$a1")
+    var b1=readLine()
+    println("$b1")
+
+    var a2="Student Class"
+    println("$a2")
+    var b2=readLine()
+    println("$b2")
+
+    var a3="Student College Name"
+    println("$a3")
+    var b3=readLine()
+    println("$b3")
+
+    var a4="Student University Name"
+    println("$a4")
+    var b4=readLine()
+    println("$b4")
+}
